4e8dbed8ce87d85c6f87da56e36abe35d7028a50
JiaMing <ray05296@gmail.com>
2024-01-07 09:18:04 +0800

    add model

--- webapp/plants/models.py
+++ webapp/plants/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-
 # Create your models here.
 
-class Plant(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    image = models.ImageField(upload_to="image/", blank=False)
-    species = models.CharField(max_length=50, null=True, blank=True)
-    invasion = models.BooleanField(blank=False)
-    upload_time = models.DateTimeField(auto_now_add=True)
 
-class Kind(models.Model):
-    species = models.CharField(primary_key=True, max_length=50)
-    description = models.TextField(max_length=100, blank= False)
+class Plant(models.Model):
+
+    species = models.CharField(max_length=100, primary_key=True)
+    name_chinese = models.CharField(max_length=30)
+    name_english = models.CharField(max_length=60)
+    isinvasive = models.BooleanField()
+    description = models.TextField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.species}, {self.isinvasive}"
+
+
+class Record(models.Model):
+
+    uploading = models.FileField(upload_to='uploads/')
+    datetime = models.DateTimeField(auto_now_add=True)
+    species = models.ForeignKey(Plant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.species.name_chinese},{self.datetime.strftime('%Y-%m-%d %H:%M')}"
+

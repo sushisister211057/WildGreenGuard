626e7589e8b9ca8eefc9ce698361d47d799148a8
JiaMing <ray05296@gmail.com>
2024-03-01 00:14:32 +0800

    add yolov8 api

--- webapp/plants/views.py
+++ webapp/plants/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse
-from django.core.exceptions import ValidationError
+import io
+import logging
+from asgiref.sync import async_to_sync
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.files.storage.filesystem import FileSystemStorage
+from django.core.files.base import ContentFile
 from .models import Plant
-from pathlib import Path
-from .identifier import preditor
-import json
-import io
+from .identifier import predictor
+from .yolopredict import yolo_predict
 from .lineapi import save_user_data, get_user_data
-from django.template import loader
-from asgiref.sync import async_to_sync, sync_to_async
-from PIL import Image
-import hashlib
+
+
 # Create your views here.
 
 pre_img = ""
+
 
 def identifier(request):
     """
@@ -29,22 +28,14 @@ def identifier(request):
         cur_img = fms.save(upload_img.name, upload_img)
         path = fms.url(cur_img)
 
-        # set global
+        # set global pre img path
         global pre_img
         if pre_img:
             fms.delete(pre_img)
         pre_img = cur_img
 
         settings.TRANS_DICT["image"] = path
-        print(path)
-
-        # wait for delete
-        # data = {"imgurl": upload_img, "scientific_name": "frog", "isinvasive": False}
-        # instance = Plant(**data)
-        # try: 
-        #     instance.full_clean()
-        # except ValidationError as e:
-        #     print(e.message_dict)
+        logging.debug(path)
 
         # create byte buffer for predict
         buffer = io.BytesIO()
@@ -52,12 +43,12 @@ def identifier(request):
             buffer.write(chunk)
         buffer.seek(0)
         img_byte = buffer.read()
-        im = Image.open(buffer)
-        print(type(img_byte), type(im))
         
-        species, isinvasive = preditor(img_byte)
+        # predict species and isinvasive 
+        species, isinvasive = predictor(img_byte)
 
-        settings.TRANS_DICT["species"] = species
+        species_name = settings.TRANS_DICT[species]
+        settings.TRANS_DICT["species_name"] = species_name
         settings.TRANS_DICT["isinvasive"] = isinvasive
 
         # save data if user is autheticated
@@ -65,32 +56,54 @@ def identifier(request):
             # do save data
             userid = request.user.userid
             # save user im to gcs
-            print(userid)
             async_to_sync(save_user_data)(userid, img_byte, species, isinvasive)
 
         return render(request, "plants/identifier.html", settings.TRANS_DICT)
 
     else:
+        settings.TRANS_DICT["species_name"] = "noinput"
         return render(request, "plants/identifier.html", settings.TRANS_DICT)
 
 
 # return yolov8 present model
-def rt_identifier(request):
-    pass
+def yolo_identifier(request):
 
-# hash_user = hashlib.sha256("userid".encode("utf-8")).hexdigest()
-# request.session[hash_user] = "123"
-# key = request.session.get(hash_user)
-# print(key)
-# userid = request.user.userid
-# userid = "Uea2325d897976dda7912258a6aa1abe3"
-# user_data = async_to_sync(get_user_data)(userid)
-# print(user_data)
-# print([name for _, name in [Plant.objects.order_by("scientific_name").values("scientific_name")]])
+    if request.method == "POST":
+        # prepare img url for display
+        upload_img = request.FILES.get("image_input_yolo")  
+
+        # create byte buffer for predict
+        buffer = io.BytesIO()
+        for chunk in upload_img.chunks(chunk_size=1024):
+            buffer.write(chunk)
+        buffer.seek(0)
+        img_byte = buffer.read()
+
+        # send image to yolo model
+        result = async_to_sync(yolo_predict)(img_byte)
+
+        # save result to storage
+        fms = FileSystemStorage()
+        cur_img = fms.save(upload_img.name, ContentFile(result))
+        path = fms.url(cur_img)
+
+        # set global pre img path
+        global pre_img
+        if pre_img:
+            fms.delete(pre_img)
+        pre_img = cur_img
+
+        settings.TRANS_DICT["image_yolo"] = path
+        logging.debug(path)
+
+        return render(request, "plants/yolo_identifier.html", settings.TRANS_DICT)
+    
+    else:
+        return render(request, "plants/yolo_identifier.html", settings.TRANS_DICT)
 
 # set index
 def index(request):
-    print("index", request.user.is_authenticated)
+    logging.debug(f"index page : {request.user.is_authenticated}")
     return render(request, "plants/index.html", settings.TRANS_DICT)
 
 # retrieve records
@@ -99,8 +112,8 @@ def records(request):
     if request.method == "GET":
         userid = request.user.userid
         user_records = async_to_sync(get_user_data)(userid)
-        print(type(user_records))
-        user_records = user_records[0]["records"]
+        user_records = user_records[0]["records"] # 要修正
+        logging.debug(user_records)
         settings.TRANS_DICT["user_records"] = user_records
         return render(request, "plants/records.html", settings.TRANS_DICT)
 
@@ -112,14 +125,13 @@ def diagram(request):
 
 # list all developers
 def developer(request):
-    print(request.user)
     return render(request, "plants/developers.html", settings.TRANS_DICT)
 
 # list all frequent asked questions
 def freq_question(request):
     return render(request, "plants/freq_questions.html", settings.TRANS_DICT)
 
-
+# href for changing language mode
 def lan_mode(request):
     # language, source path, line userid
     if request.method == "GET":

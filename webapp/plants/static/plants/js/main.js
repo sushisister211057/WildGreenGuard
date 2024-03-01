9dd78617b8567ffe2ee392cb88a4902881e7b873
JiaMing <ray05296@gmail.com>
2024-03-01 09:29:15 +0800

    update css, js and translate.json

--- webapp/plants/static/plants/js/main.js
+++ webapp/plants/static/plants/js/main.js
@@ -1,12 +1,12 @@
-function uploadtrigger(){
-    const imageInput = document.getElementById("image_input");
+function uploadtrigger(img){
+    const imageInput = document.getElementById(img);
     imageInput.click();
 };
 
-function showImg(){
-    const imageInput = document.getElementById("image_input");
-    const preimg = document.getElementById("display_image");
-    const imgbutton = document.getElementById("imgbutton");
+function showImg(img, display, btn){
+    const imageInput = document.getElementById(img);
+    const preimg = document.getElementById(display);
+    const imgbutton = document.getElementById(btn);
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
@@ -22,43 +22,3 @@ function showImg(){
 
     // console.log(imageInput.files[0]);
 };
-
-
-
-
-
-
-// imageInput.addEventListener("change", () => {
-//     const reader = new FileReader();
-    
-//     reader.addEventListener("load", () => {
-//         preimg.src = reader.result;
-//     });
-
-//     if (imageInput.files[0]) {
-//         reader.readAsDataURL(imageInput.files[0]);
-//         imgbutton.disabled = false;
-//     } else {
-//         imgbutton.disabled = true;
-//     }
-
-//     console.log(imageInput.files[0]);
-// });
-
-
-// add function to trigger file input
-// uploadBtn.addEventListener("click", ()=>{
-//     console.log("test");
-//     imageInput.click();
-// })
-
-// document.querySelector("upload_button")
-
-// function triggerFileInput() {
-//     // const imageInput = document.getElementById("image_input");
-//     console.log("test")
-//     imageInput.click();
-// }
-
-// add function to file upload when change
-
